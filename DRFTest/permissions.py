b3992864c3e3ecf002aac319c0d2dc5c8a4146b1
Mahdi Peydai <mahdi@Mahdis-MacBook-Pro.local>
2023-11-05 20:30:49 +0330

    admin permision is created and is set for author list API

--- DRFTest/permissions.py
+++ DRFTest/permissions.py
@@ -1,6 +1,11 @@
 from rest_framework import permissions
 
 
+class IsAdmin(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return request.user.is_staff and request.user
+
+
 class IsAuthorOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
--- DRFTest/views.py
+++ DRFTest/views.py
@@ -1,7 +1,7 @@
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from .serializers import *
-from .permissions import IsAuthorOrReadOnly
+from .permissions import IsAdmin, IsAuthorOrReadOnly
 
 # Creating API views for publisher model
 # the most simple one
@@ -34,6 +34,7 @@ class CategoryDetail(generics.RetrieveUpdateDestroyAPIView):
 class AuthorList(generics.ListCreateAPIView):
     queryset = Author.objects.all()
     serializer_class = AuthorSerializer
+    permission_classes = [IsAdmin]
 
 
 class AuthorDetail(generics.RetrieveUpdateDestroyAPIView):
