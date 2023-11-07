c57757f053fa75d7ded8aca24c8e531c1d5d703e
Mahdi Peydai <mahdi@Mahdis-MacBook-Pro.local>
2023-11-07 14:07:02 +0330

    initial book serializer is created

--- DRFTest/serializers.py
+++ DRFTest/serializers.py
@@ -22,3 +22,40 @@ class AuthorSerializer(ModelSerializer):
         model = Author
         fields = '__all__'
         read_only_fields = ['created_at']
+
+
+class BookSerializer(ModelSerializer):
+    author = serializers.StringRelatedField()
+    author_url = serializers.HyperlinkedRelatedField(
+        view_name='AuthorDetail',
+        queryset=Author.objects.all()
+    )
+    publisher = serializers.StringRelatedField()
+    publisher_url = serializers.HyperlinkedRelatedField(
+        view_name='PublisherDetail',
+        queryset=Publisher.objects.all()
+    )
+    categories = serializers.StringRelatedField(many=True)
+    categories_url = serializers.HyperlinkedRelatedField(
+        view_name='CategoryDetail',
+        queryset=Category.objects.all(),
+        many=True
+    )
+    readers = serializers.StringRelatedField()
+
+    class Meta:
+        model = Book
+        fields = ['id',
+                  'name',
+                  'page',
+                  'author',
+                  'author_url',
+                  'publisher',
+                  'publisher_url',
+                  'categories',
+                  'categories_url',
+                  'readers',
+                  'created_at',
+                  'updated_at']
+        read_only_fields = ['created_at',
+                            'updated_at']
