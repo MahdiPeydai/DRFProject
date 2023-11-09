86bd5a951d822e114e1243450d94abbde6f35cf3
Mahdi Peydai <mahdi@Mahdis-MacBook-Pro.local>
2023-11-09 14:02:09 +0330

    author's slug field set as read-only in admin panel

--- DRFTest/admin.py
+++ DRFTest/admin.py
@@ -2,7 +2,13 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-admin.site.register(Author)
+
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    readonly_fields = ['slug']
+
+
 admin.site.register(Book)
 admin.site.register(Publisher)
 admin.site.register(Category)
